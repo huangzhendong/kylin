68cbdcb9911364eced34926b48f28063e9e58d93
huangzhendong <21551070@zju.edu.cn>
2016-12-05 23:17:55 +0800

    2016-12-5 add some annotations

--- engine-mr/src/main/java/org/apache/kylin/engine/mr/IMRInput.java
+++ engine-mr/src/main/java/org/apache/kylin/engine/mr/IMRInput.java
@@ -26,36 +26,38 @@ import org.apache.kylin.metadata.model.TableDesc;
 
 /**
  * Any ITableSource that wishes to serve as input of MapReduce build engine must adapt to this interface.
+ * 任何想作为MapReduce输入table的数据源都必须实现这个接口
  */
 public interface IMRInput {
 
-    /** Return a helper to participate in batch cubing job flow. */
+    /** Return a helper to participate in batch cubing job flow. 获取数据块，以支持数据流*/
     public IMRBatchCubingInputSide getBatchCubingInputSide(IJoinedFlatTableDesc flatDesc);
 
-    /** Return an InputFormat that reads from specified table. */
+    /** Return an InputFormat that reads from specified table. 读取特定表InputFormat*/
     public IMRTableInputFormat getTableInputFormat(TableDesc table);
 
     /** Return an InputFormat that reads from specified table; "isFullTable" indicates whether it is a full table read or not. */
     public IMRTableInputFormat getTableInputFormat(TableDesc table, boolean isFullTable);
 
-    /** Return a helper to participate in batch cubing merge job flow. */
+    /** Return a helper to participate in batch cubing merge job flow. 用于批量cubing合并*/
     public IMRBatchMergeInputSide getBatchMergeInputSide(ISegment seg);
 
     /**
      * Utility that configures mapper to read from a table.
+     * 工具：用于配置mapper从table读入文件
      */
     public interface IMRTableInputFormat {
 
-        /** Configure the InputFormat of given job. */
+        /** Configure the InputFormat of given job. 配置job的InputFormat*/
         public void configureJob(Job job);
 
-        /** Parse a mapper input object into column values. */
+        /** Parse a mapper input object into column values. 解析table中的字段*/
         public String[] parseMapperInput(Object mapperInput);
     }
 
     /**
      * Participate the batch cubing flow as the input side. Responsible for creating
-     * intermediate flat table (Phase 1) and clean up any leftover (Phase 4).
+     * intermediate flat table (Phase 1：阶段一) and clean up any leftover (Phase 4：阶段四).
      * 
      * - Phase 1: Create Flat Table
      * - Phase 2: Build Dictionary (with FlatTableInputFormat)
@@ -64,10 +66,10 @@ public interface IMRInput {
      */
     public interface IMRBatchCubingInputSide {
 
-        /** Return an InputFormat that reads from the intermediate flat table */
+        /** Return an InputFormat that reads from the intermediate flat table 中间宽表对应的InputFormat*/
         public IMRTableInputFormat getFlatTableInputFormat();
 
-        /** Add step that creates an intermediate flat table as defined by CubeJoinedFlatTableDesc */
+        /** Add step that creates an intermediate flat table as defined by CubeJoinedFlatTableDesc 创建一张列按降序排列的FlatTable*/
         public void addStepPhase1_CreateFlatTable(DefaultChainedExecutable jobFlow);
 
         /** Add step that does necessary clean up, like delete the intermediate flat table */
@@ -76,7 +78,7 @@ public interface IMRInput {
 
     public interface IMRBatchMergeInputSide {
 
-        /** Add step that executes before merge dictionary and before merge cube. */
+        /** Add step that executes before merge dictionary and before merge cube. 在merge dic 和 cube之前要做的步骤*/
         public void addStepPhase1_MergeDictionary(DefaultChainedExecutable jobFlow);
 
     }
